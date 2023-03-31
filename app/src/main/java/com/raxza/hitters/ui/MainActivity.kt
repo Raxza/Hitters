84ab995edcd2844c443ec1ef84dcc1e34f534db6
Raja <rajaamarulloh@gmail.com>
2023-03-31 22:01:20 +0700

    Setting up MainActivity ViewModel

--- app/src/main/java/com/raxza/hitters/ui/MainActivity.kt
+++ app/src/main/java/com/raxza/hitters/ui/MainActivity.kt
@@ -2,13 +2,15 @@ package com.raxza.hitters.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.raxza.hitters.R
 import com.raxza.hitters.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,6 +18,16 @@ class MainActivity : AppCompatActivity() {
 
         binding.rvMenu.layoutManager = LinearLayoutManager(this)
 
+        setUpViewModel()
 
+        mainViewModel.menu.observe(this) { Menus ->
+            
+        }
+    }
+
+    private fun setUpViewModel() {
+        val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
+        mainViewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
+        mainViewModel.getMenu()
     }
 }
