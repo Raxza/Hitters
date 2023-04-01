4131beeb6d9d8e4396428ecfc6879e370d5796c2
Raja <rajaamarulloh@gmail.com>
2023-04-01 11:18:11 +0700

    SettingActivity and MainActivity action bar

--- app/src/main/java/com/raxza/hitters/ui/MainActivity.kt
+++ app/src/main/java/com/raxza/hitters/ui/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.raxza.hitters.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.raxza.hitters.R
 import com.raxza.hitters.data.Menu
 import com.raxza.hitters.databinding.ActivityMainBinding
 
@@ -41,5 +46,35 @@ class MainActivity : AppCompatActivity() {
         val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
         mainViewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
         mainViewModel.getMenu()
+    }
+
+    override fun onCreateOptionsMenu(menu: android.view.Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_setting -> {
+                startActivity(Intent(this, SettingActivity::class.java))
+                true
+            }
+            R.id.action_add -> {
+                val inputEditTextField = EditText(this)
+                val dialog = AlertDialog.Builder(this)
+                    .setTitle("Add new menu")
+                    .setMessage("Input menu name")
+                    .setView(inputEditTextField)
+                    .setPositiveButton("Add") { _, _ ->
+                        val editTextInput = inputEditTextField.text.toString()
+                    }
+                    .setNegativeButton("Cancel", null)
+                    .create()
+                dialog.show()
+                true
+            }
+            else -> false
+        }
     }
 }
