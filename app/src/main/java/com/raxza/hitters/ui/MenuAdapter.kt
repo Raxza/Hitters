08b06632a4d825993a6d94d37857bb921549ac54
Raja <rajaamarulloh@gmail.com>
2023-04-03 23:35:26 +0700

    create intent

--- app/src/main/java/com/raxza/hitters/ui/MenuAdapter.kt
+++ app/src/main/java/com/raxza/hitters/ui/MenuAdapter.kt
@@ -1,14 +1,28 @@
 package com.raxza.hitters.ui
 
+import android.app.Activity
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.app.ActivityOptionsCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.raxza.hitters.data.Menu
 import com.raxza.hitters.databinding.ItemMenuBinding
 
 class MainAdapter(private val menu: List<Menu>): RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
 
-    inner class MyViewHolder(val binding: ItemMenuBinding): RecyclerView.ViewHolder(binding.root)
+    inner class MyViewHolder(val binding: ItemMenuBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(menu: Menu) {
+            binding.tvDay.text = menu.name
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, DetailActivity::class.java)
+                intent.putExtra("Menu", menu)
+                itemView.context.startActivity(intent)
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val binding = ItemMenuBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -17,9 +31,7 @@ class MainAdapter(private val menu: List<Menu>): RecyclerView.Adapter<MainAdapte
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val menu = menu[position]
-        holder.binding.apply {
-            tvDay.text = menu.name
-        }
+        holder.bind(menu)
     }
 
     override fun getItemCount(): Int = menu.size
