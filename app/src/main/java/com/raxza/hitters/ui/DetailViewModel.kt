3f811c7d10430e3111b753dd1068f457b255d1ac
Raja <rajaamarulloh@gmail.com>
2023-04-04 16:42:34 +0700

    Added DetailViewModel function

--- app/src/main/java/com/raxza/hitters/ui/DetailViewModel.kt
+++ app/src/main/java/com/raxza/hitters/ui/DetailViewModel.kt
@@ -1,4 +1,20 @@
 package com.raxza.hitters.ui
 
-class DetailViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.raxza.hitters.data.HittersRepository
+import com.raxza.hitters.data.Sets
+import kotlinx.coroutines.launch
+
+class DetailViewModel(private val hittersRepository: HittersRepository): ViewModel() {
+
+    fun getSets(menuSetId: Int): LiveData<List<Sets>> = hittersRepository.getSets(menuSetId)
+
+    fun newSets(menuSetId: Int, name: String, weight: Float, rep: Int, set: Int) {
+        viewModelScope.launch {
+            val newSet = Sets(menuSetId = menuSetId, name = name, weight = weight, rep = rep, set = set)
+            hittersRepository.addSets(newSet)
+        }
+    }
 }
